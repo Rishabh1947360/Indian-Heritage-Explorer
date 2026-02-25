2756522ab83be4ea9ec2b89549f74c05e2bdbe17
Rishabh1947360 <rishabhkumar009990@gmail.com>
2026-02-25 14:03:55 +0530

    Removed base path for Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -4,20 +4,14 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/Indian-Heritage-Explorer/",   
-
   plugins: [
-    // Do NOT remove these
     react(),
     tailwindcss(),
   ],
-
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
-
-  // Do not add .css, .tsx, .ts here
   assetsInclude: ['**/*.svg', '**/*.csv'],
 })
